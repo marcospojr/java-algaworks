31504cfacdea686df2e891e18a5ffc4d5ea6841c
marcospojr <marcospojr13@gmail.com>
2020-05-14 08:54:18 -0300

    Exercicio da aula de polimorfismo

---
+++ src/com/marcosjr/fjoo/polimorfismo/Conta.java
@@ -0,0 +1,14 @@
+package com.marcosjr.fjoo.polimorfismo;
+
+public class Conta {
+
+	protected double saldo;
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+}
