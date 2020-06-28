49d4fa12418be593ff2937142d7269e64026a910
marcospojr <marcospojr13@gmail.com>
2020-06-28 17:29:58 -0300

    generics

---
+++ src/com/marcosjr/generics/RetiraElementos.java
@@ -0,0 +1,11 @@
+package com.marcosjr.generics;
+
+import java.util.List;
+
+public class RetiraElementos {
+
+	public static <T> T recuperaPrimeiro(List<T> lista) {
+		return lista.get(0);
+	}
+	
+}
